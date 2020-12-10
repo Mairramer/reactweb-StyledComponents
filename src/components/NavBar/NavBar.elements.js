062b24fa821c7eea4b1860576a219aa3fba4c09d
Mairramer <mairramer.dasilva28@hotmail.com>
2020-12-10 19:48:56 -0300

    botão signup adicionado

--- src/components/NavBar/NavBar.elements.js
+++ src/components/NavBar/NavBar.elements.js
@@ -80,7 +80,7 @@ border-bottom: 2px solid transparent;
 }
 
 @media screen and (max-width:960px){
-    height:100%;
+    width:100%;
 
     &:hover{
     border:none;
@@ -107,4 +107,23 @@ height:100%;
 
    } 
 }
+`
+export const NavItemBtn = styled.li` 
+@media screen and (max-width:960px){
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:100%;
+    height:120px;
+}
+`
+export const NavBtnLink = styled(Link)`
+display:flex;
+justify-content:center;
+align-items:center;
+text-decoration:none;
+padding: 8px 16px;
+height:100%;
+border:none;
+outline:none;
 `
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
-import { Nav, NavBarContainer, NavIcon, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavBar.elements'
+import { Button } from '../../globalStyles'
+import { Nav, NavBarContainer, NavIcon, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavItemBtn, NavBtnLink } from './NavBar.elements'
 
 
 const NavBar = () => {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click)
 
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton)
 
     return (
         <>
@@ -24,22 +39,25 @@ const NavBar = () => {
                         </MobileIcon>
                         <NavMenu onClick={handleClick} click={click}>
                             <NavItem>
-                                <NavLinks to="/">
-                                    Home
-                            </NavLinks>
+                                <NavLinks to="/">Home</NavLinks>
                             </NavItem>
-
                             <NavItem>
-                                <NavLinks to="/services">
-                                    Services
-                            </NavLinks>
+                                <NavLinks to="/services">Services</NavLinks>
                             </NavItem>
-
                             <NavItem>
-                                <NavLinks to="/products">
-                                    Products
-                            </NavLinks>
+                                <NavLinks to="/products">Products</NavLinks>
                             </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button>SIGN UP</Button>
+                                    </NavBtnLink>
+                                ) :
+                                    <NavBtnLink to="/sign-up">
+                                        <Button fontBig primary >SIGN UP</Button>
+                                    </NavBtnLink>
+                                }
+                            </NavItemBtn>
                         </NavMenu>
                     </NavBarContainer>
                 </Nav>
